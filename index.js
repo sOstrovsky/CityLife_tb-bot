5e135d493c21c97c9e1f42af926064c7d5dc911d
Сергей Островский <ostrovsky@ostrovsky.local>
2018-02-28 23:28:05 +0300

    fix

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ import { postQuery } from './helpers';
 import { find } from 'lodash'
 import moment from 'moment';
 
-const port = process.env.PORT || 8443;
 const TOKEN = config.get('token');
 
-const bot = new TelegramBot(TOKEN, {webHook: {port: port}});
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 let aboutUrl = '';
 let emailSupport = '';
